e062ed10bc9f9a7265f2e4cd6f8d63b1a7d9d55d
joewatsonsbf <joewatson40@hotmail.com>
2021-10-20 02:18:40 -0500

    first commit

---
+++ src/test/java/com/sofkau/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
